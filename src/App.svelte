cc7a0c74fdb3bb762798151e99305800f35a6c2b
rdpedraza <rdpedraza@gmail.com>
2022-11-21 22:02:53 -0400

    primer commit

---
+++ src/App.svelte
@@ -0,0 +1,16 @@
+<script>
+  let nombre = 'Diego';
+  import Boton from "./componentes/Boton.svelte";
+</script>
+
+<main>
+  <h1>Escribe tu nombre</h1> 
+  <input type="text" bind:value={nombre}>
+  <p>Hola, {nombre}!</p>
+
+  <Boton />
+</main>
+
+<style>
+
+</style>
---
+++ src/componentes/Boton.svelte
@@ -0,0 +1,15 @@
+<script>
+let contador = 0;
+$: total = contador;
+
+function clicks(){
+    contador++;
+}
+</script>
+<div class="Boton">
+    <button on:click={clicks}>Apretar</button>
+    <div>Has dado {total} clicks</div>
+</div>
+<style>
+
+</style>
